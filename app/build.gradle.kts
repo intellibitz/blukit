a63f71b659aaa39f5501b0067626e6025d73d3cc
IntelliBitz <muthu.ramadoss@gmail.com>
2026-08-12 21:50:09 +0530

    Prepare release v1.0.7 (13) for Play Store closed testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "cc.thevar.blukit"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "1.0.6"
+        versionCode = 13
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
